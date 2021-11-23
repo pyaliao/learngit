83ae86fa61f7f7f6ebe4e9c0585772166f4f879d
aliao <aliao_12345@126.com>
2021-11-23 18:03:08 +0800

    '力扣65题确定有限状态自动机解法'

--- algorithm-js/65.Valid-Number-2.js
+++ algorithm-js/65.Valid-Number-2.js
@@ -38,7 +38,7 @@
 
 // 提示：
 // 1 <= s.length <= 20
-// s 仅含英文字母（大写和小写），数字（0 - 9），加号 '+' ，减号 '-' ，或者点 '.' 
+// s 仅含英文字母（大写和小写），数字（0 - 9），加号 '+' ，减号 '-' ，或者点 '.'
 
 // 有限状态自动机
 /**
@@ -46,5 +46,103 @@
  * @return {boolean}
  */
 const isNumber = function (s) {
+  const State = {
+    STATE_INITIAL: 'STATE_INITIAL',
+    STATE_INT_SIGN: 'STATE_INT_SIGN',
+    STATE_INTEGER: 'STATE_INTEGER',
+    STATE_POINT_WITH_LEFT: 'STATE_POINT_WITH_LEFT',
+    STATE_POINT_WHITOUT_LEFT_INT: 'STATE_POINT_WHITOUT_LEFT_INT',
+    STATE_FRACTION: 'STATE_FRACTION',
+    STATE_EXP: 'STATE_EXP',
+    STATE_EXP_SIGN: 'STATE_EXP_SIGN',
+    STATE_EXP_INT: 'STATE_EXP_INT',
+    STATE_END: 'STATE_END'
+  }
+  const charType = {
+    CHAR_INT: 'CHAR_INT',
+    CHAR_EXP: 'CHAR_EXP',
+    CHAR_POINT: 'CHAR_POINT',
+    CHAR_SIGN: 'CHAR_SIGN',
+    CHAR_ILLEGAL: 'CHAR_ILLEGAL'
+  }
+  const toCharType = function (ch) {
+    if (!isNaN(Number(ch))) {
+      return charType.CHAR_INT
+    } else if (ch.toLowerCase() === 'e') {
+      return charType.CHAR_EXP
+    } else if (ch === '.') {
+      return charType.CHAR_POINT
+    } else if (ch === '+' || ch === '-') {
+      return charType.CHAR_SIGN
+    } else {
+      return charType.CHAR_ILLEGAL
+    }
+  }
 
+  // 设置状态转移规则，即当前状态可能接受的字符类型，以及接受该字符后可以到达的状态
+  // 几种状态对应几种规则
+  const initialMap = new Map()
+  initialMap.set(charType.CHAR_INT, State.STATE_INTEGER)
+  initialMap.set(charType.CHAR_POINT, State.STATE_POINT_WHITOUT_LEFT_INT)
+  initialMap.set(charType.CHAR_SIGN, State.STATE_INT_SIGN)
+
+  const intSignMap = new Map()
+  intSignMap.set(charType.CHAR_INT, State.STATE_INTEGER)
+  intSignMap.set(charType.CHAR_POINT, State.STATE_POINT_WHITOUT_LEFT_INT)
+
+  const intMap = new Map()
+  intMap.set(charType.CHAR_INT, State.STATE_INTEGER)
+  intMap.set(charType.CHAR_POINT, State.STATE_POINT_WITH_LEFT)
+  intMap.set(charType.CHAR_EXP, State.STATE_EXP)
+
+  const pointWithLeftInt = new Map()
+  pointWithLeftInt.set(charType.CHAR_INT, State.STATE_FRACTION)
+  pointWithLeftInt.set(charType.CHAR_EXP, State.STATE_EXP)
+
+  const pointWithoutLeftInt = new Map()
+  pointWithoutLeftInt.set(charType.CHAR_INT, State.STATE_FRACTION)
+
+  const fractionMap = new Map()
+  fractionMap.set(charType.CHAR_INT, State.STATE_FRACTION)
+  fractionMap.set(charType.CHAR_EXP, State.STATE_EXP)
+
+  const expMap = new Map()
+  expMap.set(charType.CHAR_SIGN, State.STATE_EXP_SIGN)
+  expMap.set(charType.CHAR_INT, State.STATE_EXP_INT)
+
+  const expSignMap = new Map()
+  expSignMap.set(charType.CHAR_INT, State.STATE_EXP_INT)
+
+  const expIntMap = new Map()
+  expIntMap.set(charType.CHAR_INT, State.STATE_EXP_INT)
+
+  const transfer = new Map()
+  transfer.set(State.STATE_INITIAL, initialMap)
+  transfer.set(State.STATE_INT_SIGN, intSignMap)
+  transfer.set(State.STATE_INTEGER, intMap)
+  transfer.set(State.STATE_POINT_WITH_LEFT, pointWithLeftInt)
+  transfer.set(State.STATE_POINT_WHITOUT_LEFT_INT, pointWithoutLeftInt)
+  transfer.set(State.STATE_FRACTION, fractionMap)
+  transfer.set(State.STATE_EXP, expMap)
+  transfer.set(State.STATE_EXP_SIGN, expSignMap)
+  transfer.set(State.STATE_EXP_INT, expIntMap)
+
+  const len = s.length
+  let currentState = State.STATE_INITIAL
+  console.log(transfer.get(currentState))
+  for (let i = 0; i < len; i++) {
+    // 如果没有相应的状态转移规则，则直接返回false
+    const type = toCharType(s[i])
+    if (!transfer.get(currentState).has(type)) {
+      return false
+    } else {
+      // 有相应的转移规则，则进行状态转移，更新状态
+      currentState = transfer.get(currentState).get(type)
+    }
+  }
+  // 判断最终的状态是否是满足条件的几个状态
+  return currentState === State.STATE_INTEGER || currentState === State.STATE_POINT_WITH_LEFT || currentState === State.STATE_FRACTION || currentState === State.STATE_EXP_INT
 }
+
+const s = '.'
+console.log(isNumber(s))
