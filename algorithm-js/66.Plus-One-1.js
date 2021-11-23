8d4a9eac08a275308b9288e2c8eec97d3d3c0c87
aliao <shujiuxiaozhu@163.com>
2021-11-23 23:42:22 +0800

    力扣66题

---
+++ algorithm-js/66.Plus-One-1.js
@@ -0,0 +1,47 @@
+// 66. 加一
+// 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+// 最高位数字存放在数组的首位，数组中每个元素只存储单个数字。
+// 你可以假设除了整数0之外，这个整数不会以零开头。
+
+// 示例 1：
+// 输入：digits = [1, 2, 3]
+// 输出：[1, 2, 4]
+// 解释：输入数组表示数字 123。
+
+// 示例 2：
+// 输入：digits = [4, 3, 2, 1]
+// 输出：[4, 3, 2, 2]
+// 解释：输入数组表示数字 4321。
+
+// 示例 3：
+// 输入：digits = [0]
+// 输出：[1]
+
+// 提示：
+// 1 <= digits.length <= 100
+// 0 <= digits[i] <= 9
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+const plusOne = function (digits) {
+  const len = digits.length
+  let sum = 0
+  for (let i = len - 1; i >= 0; i--) {
+    sum = digits[i] + 1
+    if (sum >= 10) {
+      digits[i] = sum % 10
+    } else {
+      digits[i] = sum
+      break
+    }
+  }
+  if (digits[0] === 0) {
+    digits.unshift(1)
+  }
+  return digits
+}
+
+const digits = [9]
+console.log(plusOne(digits))
